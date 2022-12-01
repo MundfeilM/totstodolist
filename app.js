211d3985009f5c1c0e4c2fa2c10f2da6dc07864f
William <william.p@electronicscience.com>
2022-12-01 14:47:26 +0800

    initial commit

--- app.js
+++ app.js
@@ -123,11 +123,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port == 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port == 3000;
+// }
 
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
